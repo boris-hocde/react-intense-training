b4d9b0e27e368c64e502fc03adce3cf7c3b5ba26
Greg Bergé <berge.greg@gmail.com>
2017-07-25 10:14:30 +0200

    [ex-2] setup layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
-const App = () => <div>Hello world!</div>
+const App = () =>
+  <div className="container">
+    <header className="header">
+      <h3>Winter is voting</h3>
+    </header>
+    <section className="content">Hello world!</section>
+  </div>
 
 export default App
