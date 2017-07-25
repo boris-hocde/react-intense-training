36286b55da84d8e07997441a31d3f8d04fd4cfe6
Greg Bergé <berge.greg@gmail.com>
2017-07-25 11:23:15 +0200

    [ex-5] add vote button

--- src/VoteCard.js
+++ src/VoteCard.js
@@ -4,8 +4,27 @@ import Card from './Card'
 class VoteCard extends React.Component {
   state = { votes: 0 }
 
+  handleClick = () => this.setState(state => ({ votes: state.votes + 1 }))
+
   render() {
-    return <Card {...this.props} footer={`${this.state.votes} votes`} />
+    return (
+      <Card
+        {...this.props}
+        text={
+          <div>
+            <p>
+              {this.props.text}
+            </p>
+            <button onClick={this.handleClick} className="btn btn-success">
+              <span role="img" aria-label="Thumb up">
+                👍🏻
+              </span>
+            </button>
+          </div>
+        }
+        footer={`${this.state.votes} votes`}
+      />
+    )
   }
 }
 
