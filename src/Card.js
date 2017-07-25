7da921cdb01f95de588bded7bc5fabf61f9d0b80
Greg Bergé <berge.greg@gmail.com>
2017-07-25 13:30:34 +0200

    [ex-11] style cards

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+  transition: transform 300ms;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`
 
 const Card = ({ picture, title, text, footer }) =>
-  <div className="card">
+  <StyledCard className="card">
     <img className="card-img-top" src={picture} alt={title} width="100%" />
     <div className="card-block">
       <h4 className="card-title">
@@ -14,6 +23,6 @@ const Card = ({ picture, title, text, footer }) =>
     <div className="card-footer text-muted">
       {footer}
     </div>
-  </div>
+  </StyledCard>
 
 export default Card
